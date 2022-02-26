b7c8436385fc7810150c021cdace494cba777e2d
ryan-son <ryan.son1002@gmail.com>
2022-02-26 11:15:30 +0900

    feat: 프로젝트 추가

---
+++ CryptocurrencyExchange/CryptocurrencyExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptocurrencyExchange
+//
+//  Created by Ryan-Son on 2022/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptocurrencyExchange/CryptocurrencyExchange/CryptocurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyExchangeApp.swift
+//  CryptocurrencyExchange
+//
+//  Created by Ryan-Son on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
