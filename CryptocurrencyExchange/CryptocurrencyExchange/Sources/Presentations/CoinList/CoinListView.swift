c222eb2014618ffd1ef0da97530722bfc69bfd6b
김정상 <topkim.dev@gmail.com>
2022-03-09 20:40:57 +0900

    -feat: `CoinListView` 오류 발생 시 다시 시도 처리 추가
    - 오류 문구 수정

--- CryptocurrencyExchange/CryptocurrencyExchange/Sources/Presentations/CoinList/CoinListView.swift
+++ CryptocurrencyExchange/CryptocurrencyExchange/Sources/Presentations/CoinList/CoinListView.swift
@@ -65,7 +65,10 @@ let coinListReducer = Reducer<
                 state.items = IdentifiedArray(uniqueElements: items)
                 return .none
             case let .failure(error):
-                return Effect(value: .showToast(message: "\(error.localizedDescription)"))
+                return Effect.merge(
+                    Effect(value: .onAppear),
+                    Effect(value: .showToast(message: "\(error.localizedDescription)"))
+                )
             }
         case .coinItem:
             return .none
@@ -142,7 +145,7 @@ fileprivate func fetchTickers(
         .mapError { error in
             Log.error("Error: \(error)")
             // TODO: 오류 났을 때 처리 필요.
-            return CoinListError.description("데이터를 받아오지 못했습니다.")
+            return CoinListError.description("다시 연결 중...")
         }
         .receive(on: DispatchQueue.main)
         .eraseToEffect()
